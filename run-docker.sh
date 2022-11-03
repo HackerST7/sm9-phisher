08c0e85d98f0697c3cedd47e1e41f9a55070c2ef
Samarth Magdum (SM9) <101000624+HackerSM9@users.noreply.github.com>
2022-11-03 13:41:34 +0530

    Update run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -9,7 +9,7 @@ if [[ ! -d "$BASE_DIR/auth" ]]; then
 fi
 
 CONTAINER="zphisher"
-IMAGE="htrtech/zphisher:latest"
+IMAGE="HackerSM9/zphisher:latest"
 IMG_MIRROR="ghcr.io/htr-tech/zphisher:latest"
 MOUNT_LOCATION=${BASE_DIR}/auth
 check_container=$(docker ps --all --format "{{.Names}}")
